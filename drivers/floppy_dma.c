e072f84ea577b552a67ec12ac3efa0a8bf04e838
Addict7 <nicolasfloquet@gmail.com>
2010-03-12 11:02:49 +0000

    [floppy] pouet
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/projet_tut_OS/trunk/tacos@147 ab7442c9-e926-469a-b309-6eee04cae01a

--- drivers/floppy_dma.c
+++ drivers/floppy_dma.c
@@ -11,6 +11,8 @@
 // Déclaration du buffer qui servira aux transfert via l'ISA DMA
 static const char floppy_dma_buffer[floppy_dmalen] __attribute__((aligned(0x8000)));
 
+static uint8_t current_cylinder = -1;
+
 void floppy_dma_init(floppy_io io_dir)
 {
 	//Pour initialiser le dma, on doit lui fournir l'adresse du buffer, ainsi que le nombre d'octets à lire
@@ -63,3 +65,38 @@ void floppy_dma_init(floppy_io io_dir)
     outb(0x02, 0x0a);   // démasque le channel 2
 	
 }
+
+int floppy_cylinder(int base, int cylinder, floppy_io io_dir)
+{
+	
+	
+	return 0;
+}
+
+void floppy_read_sector(int base, int cylinder, int head, int sector, char* buffer)
+{
+	if(cylinder != current_cylinder)
+	{
+		// Si le cylindre que l'on a en mémoire n'est pas le bon, on charge le bon
+		//floppy_read_cylinder(int base, int cylinder);
+		current_cylinder = cylinder;
+	}		
+	
+	// copier le secteur dans buffer
+}	
+
+void floppy_write_sector(int base, int cylinder, int head, int sector, char* buffer)
+{
+	if(cylinder != current_cylinder)
+	{
+		// Si le cylindre que l'on a en mémoire n'est pas le bon, on charge le bon
+		//floppy_read_cylinder(int base, int cylinder);
+		current_cylinder = cylinder;
+	}
+	
+	// copier le buffer à l'offset de floppy_dma_buffer
+	
+	//floppy_write_cylinder(int base, int cylinder);
+}
+		
+	
