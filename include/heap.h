09d8ba5b1a26f8db86d5ea360a42255df2a709ff
Smatcher <bhautboi@gmail.com>
2010-03-12 09:38:39 +0000

    debug du tas
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/projet_tut_OS/trunk/tacos@145 ab7442c9-e926-469a-b309-6eee04cae01a

--- include/heap.h
+++ include/heap.h
@@ -3,6 +3,18 @@
 
 #define HEAP_MAX_SIZE 256
 
+/*
+ * Structure de tas générique :
+ * Ce tas est compatible avec n'importe quel type de données pour peu qu'on lui
+ * donne une fonction de comparaison entre les elements.
+ * Ce tas manipule uniquement des pointeurs vers les elements qu'il contient
+ * Le tas n'est pas responsable de l'allocation et de la liberation de la mémoire
+ * Il s'agit uniquement d'une structure de tri
+ */
+
+// Une fonction de comparaison entre 2 elements
+// maxheap : la fonction doit retourner >0 si a>b, 0 si a=b, <0 si a<b
+// minheap : la fonction doit retourner <0 si a>b, 0 si a=b, >0 si a<b
 typedef int (*cmp_func_type) (void*, void*);
 
 typedef struct {
@@ -11,9 +23,16 @@ typedef struct {
 	int nb_elements;
 } heap_t;
 
+// Initialise le tas à vide et regle la fonction de comparaison
 void initHeap(heap_t* h, cmp_func_type cmp);
+
+// Recupere le pointeur du sommet, NULL sinon
 void* getTop(heap_t h);
+
+// Ajoute un element, retourne 0 en cas de succes
 int addElement(heap_t* h, void* element);
+
+// Retire le sommet, retourne 0 en cas de succes
 int removetop(heap_t* h);
 
 #endif
--- utils/heap.c
+++ utils/heap.c
@@ -1,12 +1,14 @@
 #include "heap.h"
 #include "types.h"
 
+// initialise le tas
 void initHeap(heap_t* h, cmp_func_type cmp)
 {
 	h->nb_elements = 0;
 	h->comparator = cmp;
 }
 
+// retourne le sommet
 void* getTop(heap_t h)
 {
 	void* top = NULL;
@@ -17,8 +19,19 @@ void* getTop(heap_t h)
 	return top;
 }
 
+// alterne deux elements d'indice a,b de tab
+void swap(int a, int b, void** tab)
+{
+	void* tmp;
+	tmp = tab[a];
+	tab[a] = tab[b];
+	tab[b] = tmp;
+}
+
+// ajoute une element
 int addElement(heap_t* h, void* element)
 {
+	// On verifie que le tas n'est pas plein
 	if(h->nb_elements < HEAP_MAX_SIZE)
 	{
 		int index = h->nb_elements;
@@ -31,11 +44,10 @@ int addElement(heap_t* h, void* element)
 		// On remonte l'element si necessaire
 		while(h->comparator(h->heap[index],h->heap[(index-1)/2]) > 0)
 		{
-			tmp = h->heap[index];
-			h->heap[index] = h->heap[(index-1)/2];
-			h->heap[(index-1)/2] = tmp;
-			index = (index-1)/2;
-			
+			// tant que l'element est plus grand que son pere on le remonte
+			swap(index, (index-1)/2, h->heap);
+
+			index = (index-1)/2;			
 			if(index == 0)
 				break;
 		}
@@ -48,12 +60,15 @@ int addElement(heap_t* h, void* element)
 
 int removetop(heap_t* h)
 {
+	// si le tas n'est pas deja vide
 	if(h->nb_elements > 0)
 	{
 		h->nb_elements--;
 		
+		// si on n'a pas totalement vidé le tas
 		if(h->nb_elements > 0)
 		{
+			int finished = 0;
 			int index = h->nb_elements;
 			void* tmp;
 			
@@ -61,15 +76,74 @@ int removetop(heap_t* h)
 			h->heap[0] = h->heap[index];
 			
 			// On le redescend
-			while(h->comparator(h->heap[(2*index)+1],h->heap[index]) > 0)
+			index = 0;
+			while(!finished)
 			{
-				tmp = h->heap[index];
-				h->heap[index] = h->heap[(2*index)+1];
-				h->heap[(2*index)+1] = tmp;
-				index = (2*index)+1;
+				int son1 = (2*index)+1;
+				int son2 = (2*index)+2;
 				
-				if((2*index)+1 >= h->nb_elements)
-					break;
+				// si on a un fils
+				if(son1 < h->nb_elements)
+				{
+					// si on a un second fils
+					if(son2 < h->nb_elements)
+					{
+						// On a deux fils
+						if(h->comparator(h->heap[son1],h->heap[index]) > 0)
+						{
+							if(h->comparator(h->heap[son2],h->heap[son1]) > 0)
+							{
+								// moi < fils1 < fils2
+								// on alterne moi et fils2
+								swap(index, son2, h->heap);
+								index = son2;
+							}
+							else
+							{
+								// moi < fils2 < fils1
+								// on alterne moi et fils1
+								swap(index, son1, h->heap);
+								index = son1;
+							}
+						}
+						else if(h->comparator(h->heap[son2],h->heap[index]) > 0)
+						{
+							// fils1 < moi < fils2
+							// on alterne moi et fils2
+							swap(index, son2, h->heap);
+							index = son2;
+						}
+						else
+						{
+							// fils1 & fils2 < moi
+							// on a fini de descendre
+							finished = 1;
+						}
+					}
+					else
+					{
+						// On a un seul fils
+						if(h->comparator(h->heap[son1],h->heap[index]) > 0)
+						{
+							// fils > moi
+							// On alterne
+							swap(index, son1, h->heap);
+							index = son1;
+						}
+						else
+						{
+							// moi > fils
+							// on a fini de descendre
+							finished = 1;
+						}
+					}
+				}
+				else
+				{
+					// Plus de fils
+					// on a fini de descendre
+					finished = 1;
+				}
 			}
 		}
 		return 0;
