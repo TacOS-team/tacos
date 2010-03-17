df8e5b89ea3eebdd2b2ead084ac4feac02054236
Addict7 <nicolasfloquet@gmail.com>
2010-03-17 18:04:04 +0000

    passage des fonctions d'acces au msr en macro
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/projet_tut_OS/trunk/tacos@194 ab7442c9-e926-469a-b309-6eee04cae01a

--- kernel/include/msr.h
+++ kernel/include/msr.h
@@ -5,7 +5,18 @@
 #define MSR_SYSENTER_ESP	0x0175
 #define MSR_SYSTENTE_EIP	0x0176
 
-void write_msr(uint32_t reg, uint32_t msr_high, uint32_t msr_low);
-void read_msr(uint32_t reg, uint32_t* msr_high, uint32_t msr_low);
+#define write_msr(reg, msr_low, msr_high) 	\
+		__asm__ volatile (					\
+			"wrmsr\n\t"						\
+			: /* No output */				\
+			: "c" (reg), "a" (msr_low), "d" (msr_high))
+			
+#define read_msr(reg, msr_low, msr_high) 		\
+		__asm__ volatile (						\
+			"rdmsr\n\t"							\
+			: "=a" (*msr_low), "=d" (*msr_high)	\
+			: "c" (reg))
+	
+		
 
 #endif
