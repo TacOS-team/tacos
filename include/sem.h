0126b8c5ace1df1c343f7e91ee99e5618c25f793
Smatcher <bhautboi@gmail.com>
2010-05-24 23:02:51 +0000

    git-svn-id: https://www.etud.insa-toulouse.fr/svn/projet_tut_OS/trunk/tacos@412 ab7442c9-e926-469a-b309-6eee04cae01a

--- include/sem.h
+++ include/sem.h
@@ -1,10 +1,37 @@
 #ifndef SEM_H
 #define SEM_H
 
+/** 
+* @file sem.h 
+*/
+
 #include <types.h>
 
+/** 
+* @brief donne l'identifiant (semid) d'un sémaphore déjà existant
+* 
+* @param key la clef du sémaphore, cette clef est commune au système
+* 
+* @return le semid correspondant au sémaphore, -1 si une erreur s'est produite.
+*/
 int semget(uint8_t key);
+
+/** 
+* @brief crée un sémaphore
+* 
+* @param key la clef du sémaphore
+* 
+* @return le semid correspondant au sémaphore, -1 si une erreur s'est produite.
+*/
 int semcreate(uint8_t key);
+
+/** 
+* @brief supprime un sémaphore
+* 
+* @param semid l'identifiant du sémaphore à supprimer.
+* 
+* @return 
+*/
 int semdel(uint32_t semid);
 int semP(uint32_t semid);
 int semV(uint32_t semid);
