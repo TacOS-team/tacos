259dff1a7b6a111c52d4a75747a6ab96dba43eb6
MaximeCheramy <maxime81@gmail.com>
2010-06-05 23:32:25 +0000

    Ajout du schéma de pagination. Et c'est classe.
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/projet_tut_OS/trunk/tacos@496 ab7442c9-e926-469a-b309-6eee04cae01a

--- apps/pres.c
+++ apps/pres.c
@@ -2,6 +2,26 @@
 #include <gui.h>
 #include <process.h>
 
+struct widget_t* schema_pagination(struct window_t *win) {
+return addTxt(win, "Adresse : ########## ########## ############\n"
+"          <--------> <--------> <---------->\n"
+"            |             |________    |_______________\n"
+"            |            Indice de |        Offset dans|\n"
+"            |             la page  |         la page   |\n"
+"      Indice|    ________          |    ________       |    ________ \n"
+"      de la |   |        |         |   |        |      |   |        |\n"
+"      table |   |________|         |   |________|      |   |        |\n"
+"            |   | @ phys |         |   | @ phys |      |   |        |\n"
+"            |-->|  table |--|      |-->|  page  |---|  |-->|________|\n"
+"                |________|  |          |________|   |      |        |\n"
+"                |        |  |          |        |   |      |        |\n"
+"                |........|  |          |........|   |      |........|\n"
+"                |        |  |          |        |   |      |        |\n"
+"          CR3-->|________|  |--------->|________|   |----->|________|\n"
+"               Repertoire de         Table de pages       Page (4 kio)\n"
+"              tables de pages        (1024 entrees)\n");
+}
+
 int main_pres(int argc __attribute__ ((unused)), char* argv[] __attribute__ ((unused)))
 {
 	struct window_t* win;
@@ -13,10 +33,11 @@ int main_pres(int argc __attribute__ ((unused)), char* argv[] __attribute__ ((un
 
 	bck = addButton(win,"Prec");
 	nxt = addButton(win,"Suiv");
-	txt = addTxt(win, "Bonjour, voici blabla nininninininininininininininininfefjkrjlknvfnflbngfklbjgkbjklgbjgkbjgklb\nEt nana, et sisi ...\nEt vlan et bim !!!!");
-	setWidgetProperties(bck,  1 , 20, 3, 6, 5, 0);
-	setWidgetProperties(nxt, 70 , 20, 3, 6, 5, 0);
-	setWidgetProperties(txt, 1 , 1, 18, 78, 3, 0);
+	//txt = addTxt(win, "Bonjour, voici blabla nininninininininininininininininfefjkrjlknvfnflbngfklbjgkbjklgbjgkbjgklb\nEt nana, et sisi ...\nEt vlan et bim !!!!");
+	txt = schema_pagination(win);
+	setWidgetProperties(bck,  1 , 21, 3, 6, 5, 0);
+	setWidgetProperties(nxt, 73 , 21, 3, 6, 5, 0);
+	setWidgetProperties(txt, 1 , 1, 19, 78, 7, 0);
 
 	runWindow(win);
 	
