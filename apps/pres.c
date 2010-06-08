99cf1b6ee7fcd9e43b08126e6037c4ecd4024191
lrigal <lrigal@etud.insa-toulouse.fr>
2010-06-08 14:17:50 +0000

    git-svn-id: https://www.etud.insa-toulouse.fr/svn/projet_tut_OS/trunk/tacos@518 ab7442c9-e926-469a-b309-6eee04cae01a

--- apps/pres.c
+++ apps/pres.c
@@ -2,7 +2,7 @@
 #include <gui.h>
 #include <process.h>
 
-#define NB_SLIDES 2
+#define NB_SLIDES 3
 
 int current = 0;
 struct widget_t* title[NB_SLIDES];
@@ -31,7 +31,7 @@ struct widget_t* schema_vmm(struct window_t *win, struct widget_t** txt, struct 
 	*title = addButton(win,"VMM");
 }
 
-void schema_pagination(struct window_t *win, struct widget_t** txt, struct widget_t** title) {
+struct widget_t* schema_pagination(struct window_t *win, struct widget_t** txt, struct widget_t** title) {
 	*txt = addTxt(win, "Adresse : \033[32m########## \033[31m########## \033[34m############\033[0m\n"
 "          <--------> <--------> <---------->\n"
 "            |             |________    |_______________\n"
@@ -50,6 +50,26 @@ void schema_pagination(struct window_t *win, struct widget_t** txt, struct widge
 "               Repertoire de         Table de pages       Page (4 kio)\n"
 "              tables de pages        (1024 entrees)\n");
 	*title = addButton(win,"Pagination");
+}
+
+struct widget_t* schema_ordonnanceur(struct window_t *win, struct widget_t** txt, struct widget_t** title) {
+	*txt = addTxt(win, 
+"\n"
+"      \033[30m               \033[30m                                    \033[30mEBP___\033[30m  \n"
+"    pi\033[30mle kernel -----\033[30m->                                  \033[30m      \033[30m|  \n"
+"  ____\033[32m __ __ __ __ __\033[34m __ __ __ __ __ __ __ __ __ __ __ __\033[31m __ __\033[30mV__\n"
+"      \033[32m|  |  |  |  |  \033[34m|  |  |  |  |k |  |  |  |  |  |  |  \033[31m|k |k \033[30m|  \n"
+"      \033[32m|s |e |e |c |e \033[34m|e |e |e |e |e |e |e |e |d |e |f |g \033[31m|e |e \033[30m|  \n" 
+"    ..\033[32m|s |s |f |s |i \033[34m|a |c |d |b |s |b |s |d |s |s |s |s \033[31m|i |b \033[30m|..\n"
+"      \033[32m|  |p |l |  |p \033[34m|x |x |x |x |p |p |i |i |  |  |  |  \033[31m|p |p \033[30m|  \n"
+"  ____\033[32m|__|__|__|__|__\033[34m|__|__|__|__|__|__|__|__|__|__|__|__\033[31m|__|__\033[30m|__\n"
+"      \033[30m |           | \033[30m |                                 |\033[30m |   |\033[30m   \n"
+"      \033[30m |___________| \033[30m |_________________________________|\033[30m |___|\033[30m   \n"
+"      \033[30m       |       \033[30m                 |                  \033[30m   |             \n"
+"      \033[32mempilé par le  \033[34m    empilé après l'interruption     \033[31m empilé à        \n"
+"      \033[32mmécanisme d'   \033[34m                                    \033[31m l'appel de      \n"
+"      \033[32minterruption   \033[34m                                    \033[31m l'ordonnanceur\033[30m\n");
+	*title = addButton(win,"Ordonnanceur");
 }
 
 void goBack(struct widget_t* wdg, int x, int y)
@@ -92,6 +112,7 @@ int main_pres(int argc __attribute__ ((unused)), char* argv[] __attribute__ ((un
 
 	schema_pagination(win,&txt[0],&title[0]);
 	schema_vmm(win,&txt[1],&title[1]);
+	schema_ordonnanceur(win,&txt[2],&title[2]);
 
 	for(i=0 ; i<NB_SLIDES ; i++)
 	{
