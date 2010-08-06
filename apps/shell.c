d4274f16c40a91764c81fc163503dcbfb0e63ba4
Addict7 <nicolasfloquet@gmail.com>
2010-08-06 13:30:50 +0000

    git-svn-id: https://www.etud.insa-toulouse.fr/svn/projet_tut_OS/trunk/tacos@624 ab7442c9-e926-469a-b309-6eee04cae01a

--- apps/shell.c
+++ apps/shell.c
@@ -137,16 +137,68 @@ static int sleep_shell()
 	return 0;
 }
 
+int ps()
+{
+	process_t* aux = get_process_list(FIRST_PROCESS);
+
+	const int clk_per_ms = CLOCKS_PER_SEC / 1000;
+	long int ms;
+	int s;
+	int m;
+	int h;
+
+	printf("pid\tname\t\ttime\t\t%CPU\tstate\n");   
+	while(aux!=NULL)
+	{
+			
+			/* Calcul du temps d'execution du processus */
+			ms = aux->user_time / clk_per_ms;
+			s = ms / 1000;
+			
+			m = s / 60;
+			s = s % 60;
+			h = m / 60;
+			m = m % 60;
+			
+			/*if (aux->process == active_process) {
+					printf("*");
+			}*/
+			
+			printf("%d\t%s\t\t%dh %dm %ds\t%d\%\t",aux->pid, aux->name, h, m ,s, (int)(((float)aux->last_sample/(float)CPU_USAGE_SAMPLE_RATE)*100.f));
+			
+			switch(aux->state)
+			{
+					case PROCSTATE_IDLE:
+							printf("IDLE\n");
+							break;
+					case PROCSTATE_RUNNING:
+							printf("RUNNING\n");
+							break;
+					case PROCSTATE_WAITING:
+							printf("WAITING\n");
+							break;
+					case PROCSTATE_TERMINATED:
+							printf("TERMINATED\n");
+							break;
+					default:
+							break;
+			}
+			
+			aux = get_process_list(NEXT_PROCESS);
+	}
+	return 0;
+}
+
 int top(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused)))
 {
 	disable_cursor(1);
-	printf("TODO: recoder ps proprement...");
-	/*while(1)
+	
+	while(1)
 	{
 		printf("\033[1;1H");
-		//print_process_list();
+		ps();
 		sleep(1);
-	}*/
+	}
 }
 static int exec_top()
 {
@@ -189,7 +241,7 @@ int shell(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused)
 	add_builtin_cmd(test_fputs, "test_fputs");
 	add_builtin_cmd(test_fwrite, "test_fwrite");
 	add_builtin_cmd(test_fread, "test_fread");
-	add_builtin_cmd((func_ptr)exec_top, "ps"); /*TODO: Fournir les interfaces avec kprocess pour pouvoir faire ça */
+	add_builtin_cmd(ps, "ps");
 	//add_builtin_cmd(kmalloc_print_mem, "kmalloc_print_mem");
 	/*add_builtin_cmd((func_ptr)test_kmalloc, "test_kmalloc");*/
 	/*add_builtin_cmd((func_ptr)test_memory_reserve_page_frame, "test_reserve_frame");*/
