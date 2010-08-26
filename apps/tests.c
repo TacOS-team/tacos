bc1451359574685867d57556591e9485dd2a7079
MaximeCheramy <maxime81@gmail.com>
2010-08-26 01:02:04 +0000

    Ajout de quelques tests unitaires pour fseek. refs #1
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/projet_tut_OS/trunk/tacos@653 ab7442c9-e926-469a-b309-6eee04cae01a

--- apps/tests.c
+++ apps/tests.c
@@ -21,14 +21,12 @@
 
 static int color;
 
-static void handleClick(struct widget_t* wdg, int x __attribute__ ((unused)), int y __attribute__ ((unused)))
-{
-	color = (color+1)%8;
-	setWidgetProperties(wdg, 2, 2, 5, 10+color, color, 6);
+static void handleClick(struct widget_t* wdg, int x __attribute__ ((unused)), int y __attribute__ ((unused))) {
+	color = (color + 1) % 8;
+	setWidgetProperties(wdg, 2, 2, 5, 10 + color, color, 6);
 }
 
-int gui_task(int argc __attribute__ ((unused)), char* argv[] __attribute__ ((unused)))
-{
+int gui_task(int argc __attribute__ ((unused)), char* argv[] __attribute__ ((unused))) {
 	color = 0;
 	struct window_t* win;
 	static struct widget_t* wdg;
@@ -42,9 +40,8 @@ int gui_task(int argc __attribute__ ((unused)), char* argv[] __attribute__ ((unu
 	return 0;
 }
 
-int test_gui()
-{
-	exec((paddr_t)gui_task,"tache_gui" );
+int test_gui() {
+	exec((paddr_t) gui_task, "tache_gui");
 	return 0;
 }
 
@@ -60,7 +57,7 @@ int test_fread() {
 	FILE *file = fopen("fd0:/doc.txt", "r");
 
 	char buffer[100];
-	fread(buffer, sizeof(char),sizeof(buffer),  file);
+	fread(buffer, sizeof(char), sizeof(buffer), file);
 
 	buffer[99] = '\0';
 	printf("%s", buffer);
@@ -69,150 +66,167 @@ int test_fread() {
 	return 0;
 }
 
-int test_fseek() {
+void tu_seek0(const char * nom_test, const char * resultat_attendu, long offset) {
 	FILE *file = fopen("fd0:/doc.txt", "r");
+	int c;
 
-	char buffer[50];
-	fread(buffer, sizeof(buffer), sizeof(char), file);
+	char buffer[11];
+	fseek(file, offset, SEEK_SET);
 
-	buffer[49] = '\0';
-	printf("50 premiers caractères : %s\n", buffer);
+	if (fread(buffer, sizeof(buffer), sizeof(char), file) > 0) {
+		buffer[10] = '\0';
+	} else {
+		buffer[0] = '\0';
+	}
 
-	fseek(file, 10, SEEK_SET);
-	fread(buffer, sizeof(buffer), sizeof(char), file);
+	if (strcmp(buffer, resultat_attendu) == 0) {
+		printf("%s [OK]\n", nom_test);
+	} else {
+		printf("%s [failed] :(\n", nom_test);
+		printf("Attendu : %s\n Obtenu : %s\n", resultat_attendu, buffer);
+		printf("Ou encore : ");
+		for (c = 0; c < 11; c++) {
+			printf("(%d)", buffer[c]);
+		}
+		printf("\n Attendu : ");
+		for (c = 0; c < 11; c++) {
+			printf("(%d)", resultat_attendu[c]);
+		}
+		printf("\n");
+	}
+}
 
-	buffer[49] = '\0';
-	printf("50 caractères après fseek de 10 (depuis 0). : %s\n", buffer);
+int test_fseek() {
+	tu_seek0("fseek debut+10", "86 refers ", 10);
+	tu_seek0("fseek debut+100", "as launche", 100);
+	tu_seek0("fseek debut+1000", "f servers ", 1000);
+	tu_seek0("fseek debut+10000", "", 10000);
 
 	//fclose(file);
 	return 0;
 }
 
-int sem_t2(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused)))
-{
+int sem_t2(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused))) {
 	int semid1 = semget(4);
 	int semid2 = semget(42);
 	int i;
-	while(1) {
-			semP(semid1);
-			for(i=0; i<0x005FFFFE; i++){}
-			printf("T2:Pong!\n");
-			semV(semid2);
-			
+	while (1) {
+		semP(semid1);
+		for (i = 0; i < 0x005FFFFE; i++) {
+		}
+		printf("T2:Pong!\n");
+		semV(semid2);
+
 	}
 
 	return 0;
 }
 
-int sem_t1(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused)))
-{
+int sem_t1(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused))) {
 	int semid1 = semget(4);
 	int semid2 = semget(42);
 	unsigned int i = 0;
 
-	while(1){
+	while (1) {
 		semP(semid2);
-		for(i=0; i<0x005FFFFE; i++){}
+		for (i = 0; i < 0x005FFFFE; i++) {
+		}
 		printf("T1:Ping?\n");
 		semV(semid1);
 	}
 	return 0;
 }
 
-int test_semaphores()
-{
+int test_semaphores() {
 	/* Test des sémaphores: Deux taches effectuant un calcul (ici un boucle) s'alterne
 	 * Pour se convaincre de l'efficacité, on peut utilise la commande "ps" pour constater
 	 * que les deux tachent deviennent bien inactives (et donc ne consomment pas de CPU) l'une 
 	 * après l'autre
 	 */
-	int semid1 = semcreate(4); 
+	int semid1 = semcreate(4);
 	int semid2;
-	
+
 	semid2 = semcreate(42);
 	semP(semid1);
 
-	exec((paddr_t)sem_t1,"ping");
-	exec((paddr_t)sem_t2,"pong");
-	
+	exec((paddr_t) sem_t1, "ping");
+	exec((paddr_t) sem_t2, "pong");
+
 	return 0;
 }
 
 void test_memory_reserve_page_frame() {
 	int i;
 	scanf("%d", &i);
-	while(i--)
+	while (i--)
 		printf("Cadre de page réservé : %d\n", memory_reserve_page_frame());
 }
 
-int tu_rand()
-{
-    int tab[100];
-    int i;
-    int n_tirages = 1000000;
-    int n;
-    char *nom_test = "rand";
+int tu_rand() {
+	int tab[100];
+	int i;
+	int n_tirages = 1000000;
+	int n;
+	char *nom_test = "rand";
 
-    // init
-    for (i = 0; i < 100; i++) {
-        tab[i] = 0;
-    }
+	// init
+	for (i = 0; i < 100; i++) {
+		tab[i] = 0;
+	}
 
-    // tirages
-    for (n = 0; n < n_tirages; n++) {
-        i = (double)rand() / ((double)(RAND_MAX) + 1.0) * 100.0;
-        tab[i]++;
-    }
+	// tirages
+	for (n = 0; n < n_tirages; n++) {
+		i = (double) rand() / ((double) (RAND_MAX) + 1.0) * 100.0;
+		tab[i]++;
+	}
 
-    double Q = 0;
-    for (i = 0; i < 100; i++) {
-        Q += (double)((tab[i] - 10000) * (tab[i] - 10000)) / 10000.0;
-    }
+	double Q = 0;
+	for (i = 0; i < 100; i++) {
+		Q += (double) ((tab[i] - 10000) * (tab[i] - 10000)) / 10000.0;
+	}
 
-    if (Q < 123.23) { // 5% d'erreur (http://www.apprendre-en-ligne.net/random/tablekhi2.html)
-		printf("%s [OK] (%d.%d)\n", nom_test, (int)Q, (int)((Q - (int)Q)*100));
-        return 1;
-    } else {
+	if (Q < 123.23) { // 5% d'erreur (http://www.apprendre-en-ligne.net/random/tablekhi2.html)
+		printf("%s [OK] (%d.%d)\n", nom_test, (int) Q, (int) ((Q - (int) Q)
+				* 100));
+		return 1;
+	} else {
 		printf("%s [failed] :(\n", nom_test);
-		printf("Attendu : < 123.23\n Obtenu : %d.%d\n", (int)Q, (int)((Q - (int)Q)*100));
+		printf("Attendu : < 123.23\n Obtenu : %d.%d\n", (int) Q, (int) ((Q
+				- (int) Q) * 100));
 		printf("Ou encore : ");
-        return 0;
-    }
+		return 0;
+	}
 }
 
-int pi(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused)))
-{
+int pi(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused))) {
 	const double prec = 0.0001;
 	double x = 0.0, y = 0.0;
 	double res;
 	int p_in = 0, p_tot = 0;
-	for(x=0; x<1.0; x+=prec)
-	{
-		for(y=0; y<1.0; y+=prec)
-		{
-			if(x*x+y*y<1.0)
+	for (x = 0; x < 1.0; x += prec) {
+		for (y = 0; y < 1.0; y += prec) {
+			if (x * x + y * y < 1.0)
 				p_in++;
-			
+
 			p_tot++;
 		}
 	}
-	res = 4.0*(double)p_in/(double)p_tot;
+	res = 4.0 * (double) p_in / (double) p_tot;
 
-	printf("Tesultat tache: P_in=%d, P_tot=%d, res=%d\n", p_in, p_tot,(int)(res*1000.0));
+	printf("Tesultat tache: P_in=%d, P_tot=%d, res=%d\n", p_in, p_tot,
+			(int) (res * 1000.0));
 
 	return 0;
 }
 
-int test_task1(int argc , char** argv __attribute__ ((unused)))
-{	
+int test_task1(int argc, char** argv __attribute__ ((unused))) {
 	/*while(1)
-		printf("Coucou task1\n");*/
-	
+	 printf("Coucou task1\n");*/
+
 	return 0;
 }
 
-int test_task()
-{
+int test_task() {
 	exec(test_task1, "test");
 	//create_process("test",(paddr_t)test_task1,"il est plus d'une heure du matin et je suis fatigué",0x1000,3);
 	//create_process_test("test", (paddr_t)test_task1, 0x100 , "1 2", 0x1000, 3);
@@ -220,14 +234,12 @@ int test_task()
 	return 0;
 }
 
-int calc_pi()
-{
-	exec((paddr_t)pi, "pi");
+int calc_pi() {
+	exec((paddr_t) pi, "pi");
 	return 0;
 }
 
-int test_mouse_task()
-{
+int test_mouse_task() {
 	int i = 0;
 	int x;
 	int y;
@@ -239,34 +251,28 @@ int test_mouse_task()
 	int btn2_frz = 0;
 	printf("\n---- Test Mouse ----\n");
 
-	while(1)
-	{
-		if(i%100000 == 0)
-		{
-			getMouseCoord(&x,&y);
-			
-			if(getMouseBtn(0))
-			{
-				if(!btn1_frz) col1 = (col1+1)%0xF;
+	while (1) {
+		if (i % 100000 == 0) {
+			getMouseCoord(&x, &y);
+
+			if (getMouseBtn(0)) {
+				if (!btn1_frz)
+					col1 = (col1 + 1) % 0xF;
 				btn1_frz = 1;
-			}
-			else
-			{
+			} else {
 				btn1_frz = 0;
 			}
-			
-			if(getMouseBtn(1))
-			{
-				if(!btn2_frz) col2 = (col2+1)%0xF;
+
+			if (getMouseBtn(1)) {
+				if (!btn2_frz)
+					col2 = (col2 + 1) % 0xF;
 				btn2_frz = 1;
-			}
-			else
-			{
+			} else {
 				btn2_frz = 0;
 			}
-			
-			x = x*80/640;
-			y = 25 - (y*25/480);
+
+			x = x * 80 / 640;
+			y = 25 - (y * 25 / 480);
 			// XXX: 
 			//set_attribute_position(0, col2, old_x, old_y);
 			//set_attribute_position(col1, col2, x, y);
@@ -275,18 +281,17 @@ int test_mouse_task()
 		}
 		i++;
 	}
-	while(1);
+	while (1)
+		;
 	return 0;
 }
 
-int test_mouse()
-{
-	exec((paddr_t)test_mouse_task,"mouse");
+int test_mouse() {
+	exec((paddr_t) test_mouse_task, "mouse");
 	return 0;
 }
 
-int test_scanf()
-{
+int test_scanf() {
 	printf("Entre un mot : \n");
 	char b[100];
 	scanf("%s", b);
@@ -294,8 +299,7 @@ int test_scanf()
 	return 0;
 }
 
-int test_fgets()
-{
+int test_fgets() {
 	printf("Entre une phrase : \n");
 	char b[100];
 	fgets(b, sizeof(b), stdin);
@@ -303,24 +307,12 @@ int test_fgets()
 	return 0;
 }
 
-int test_fputs()
-{
+int test_fputs() {
 	fputs("Hello world!\n", stdout);
 	return 0;
 }
 
-/*
-int test_fwrite() 
-{
-	char chaine[] = "Hello world (fwrite)!\n";
-	fwrite(chaine, 2, sizeof(chaine)/2, stdout);
-	return 0;
-}
-*/
-	
-
-int test_ansi()
-{
+int test_ansi() {
 	printf("\033[2J");
 	fflush(stdout);
 	printf("\033[8;20H\033[33m=====");
@@ -331,41 +323,36 @@ int test_ansi()
 	return 0;
 }
 
-int proc_write_serial(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused)))
-{
+int proc_write_serial(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused))) {
 	char buffer[80];
 	printf(">");
-	while(1)
-	{
-		
+	while (1) {
+
 		fflush(stdout);
-		scanf("%s",buffer);
-	
+		scanf("%s", buffer);
+
 		serial_puts(COM1, buffer);
 		serial_putc(COM1, ' ');
 	}
 	return 0;
 }
 
-int test_write_serial()
-{
-	exec((paddr_t)proc_write_serial, "wserial");
+int test_write_serial() {
+	exec((paddr_t) proc_write_serial, "wserial");
 	return 0;
 }
 
-int proc_read_serial(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused)))
-{
+int proc_read_serial(int argc __attribute__ ((unused)), char** argv __attribute__ ((unused))) {
 	char buffer[64];
-	
-	while(1)
-	{
+
+	while (1) {
 		serial_gets(COM1, buffer, 64);
-		printf("%s",buffer);
+		printf("%s", buffer);
 		fflush(stdout);
 	}
 }
 
-void tu_ctype(const char * nom_test, int (*p)(), const char * resultat_attendu) {
+void tu_ctype(const char * nom_test, int(*p)(), const char * resultat_attendu) {
 	char buf[256];
 	buf[0] = '\0';
 	int len = 0;
@@ -393,28 +380,26 @@ void tu_ctype(const char * nom_test, int (*p)(), const char * resultat_attendu) 
 	}
 }
 
-
-int tu_sscanf()
-{
+int tu_sscanf() {
 	char * str = "World 42 Hello";
 	char s[10];
 	char s2[10];
 	int d;
-    char *resultat_attendu = "TU : Hello (42) World";
+	char *resultat_attendu = "TU : Hello (42) World";
 	char buf[80];
 
 	sscanf(str, "%s %d %s", s, &d, s2);
 
 	sprintf(buf, "TU : %s (%d) %s", s2, d, s);
 
-    if (strcmp(buf, resultat_attendu) == 0) {
-        printf("%s [OK]\n", "sscanf");
-        return 1;
-    } else {
-        printf("%s [failed] :(\n", "sscanf");
-        printf("Attendu : %s\n Obtenu : %s\n", resultat_attendu, buf);
-        return 0;
-    }
+	if (strcmp(buf, resultat_attendu) == 0) {
+		printf("%s [OK]\n", "sscanf");
+		return 1;
+	} else {
+		printf("%s [failed] :(\n", "sscanf");
+		printf("Attendu : %s\n Obtenu : %s\n", resultat_attendu, buf);
+		return 0;
+	}
 }
 
 int tu_sprintf() {
@@ -425,7 +410,8 @@ int tu_sprintf() {
 	char buf[80];
 	int r = sprintf(buf, "TU : %s (%d) %s", t, d, t2);
 
-	if (strcmp(buf, resultat_attendu) == 0 && r == (int)strlen(resultat_attendu)) {
+	if (strcmp(buf, resultat_attendu) == 0 && r == (int) strlen(
+			resultat_attendu)) {
 		printf("%s [OK]\n", "sprintf");
 		return 1;
 	} else {
@@ -437,41 +423,41 @@ int tu_sprintf() {
 	}
 }
 
-int test_ctype()
-{
+int test_ctype() {
 	tu_ctype("isupper", isupper, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 	tu_ctype("islower", islower, "abcdefghijklmnopqrstuvwxyz");
-	tu_ctype("isalpha", isalpha, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+	tu_ctype("isalpha", isalpha,
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
 	tu_ctype("isdigit", isdigit, "0123456789");
-	tu_ctype("isalnum", isalnum, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+	tu_ctype("isalnum", isalnum,
+			"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
 	tu_ctype("ispunct", ispunct, "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
 	tu_ctype("isspace", isspace, "\t\n\v\f\r ");
-	tu_ctype("isprint", isprint, " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~");
+	tu_ctype(
+			"isprint",
+			isprint,
+			" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~");
 
 	// TODO : compléter.
 
 	return 0;
 }
 
-int test_stdio()
-{
+int test_stdio() {
 	tu_rand(); // TODO : le bouger ailleurs.
 
-	tu_sprintf()
-    && tu_sscanf();
+	tu_sprintf() && tu_sscanf();
 }
 
-int test_read_serial()
-{
-	exec((paddr_t)proc_read_serial, "rserial");
+int test_read_serial() {
+	exec((paddr_t) proc_read_serial, "rserial");
 	return 0;
 }
 
 char programme[0x3000];
 
 //#define TEST_EXEC
-int test_elf()
-{
+int test_elf() {
 	FILE* fd = NULL;
 
 #ifdef TEST_EXEC
@@ -484,17 +470,14 @@ int test_elf()
 	//fd = fopen("fd0:/system/kernel.bin", "r");
 	fd = fopen("fd0:/a.out", "r");
 
-	if(fd == NULL)
-	{
+	if (fd == NULL) {
 		printf("failed.\n");
-	}
-	else
-	{
+	} else {
 		printf("Ok!\n");
 #ifndef TEST_EXEC
 		elf_info(fd);
 #endif
-		
+
 #ifdef	TEST_EXEC
 		entry = load_elf(fd, programme);
 		pp = &(programme[entry]);
@@ -503,5 +486,5 @@ int test_elf()
 #endif
 	}
 	/*close(fd);*/
-	return 0;	
+	return 0;
 }
