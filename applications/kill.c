2221027a7c121a85ef67d05368ae22de50be06df
Maxime Chéramy <maxime81@gmail.com>
2011-10-20 20:38:11 +0200

    Ajout d'une commande kill (un peu à l'arrache, mais mieux que rien).

---
+++ applications/kill.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+	if (argc < 3) {
+		printf("Usage: %s SIGNAL PID\n", argv[0]);
+		return 1;
+	}
+	pid_t pid = atoi(argv[1]);
+	return kill(pid, atoi(argv[2]));
+}
