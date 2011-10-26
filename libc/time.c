811fdda3fe6aa8aa53ca6ff8994290955b625279
Maxime Chéramy <maxime81@gmail.com>
2011-10-26 23:14:21 +0200

    Du code de clock.c est passé dans time.c. Reste à faire les syscalls.

--- libc/time.c
+++ libc/time.c
@@ -34,7 +34,31 @@
 
 #include <time.h>
 #include <stdio.h>
-//#include <clock.h>
+#include <stdlib.h>
+
+#define MILLISECONDS_PER_SECOND 1000
+#define SECONDS_PER_MINUTE 60
+#define MINUTES_PER_HOUR 60
+#define HOURS_PER_DAY 24
+#define MONTH_PER_YEAR 12
+
+#define MILLISECONDS_PER_MINUTE MILLISECONDS_PER_SECOND*SECONDS_PER_MINUTE
+#define MILLISECONDS_PER_HOUR MILLISECONDS_PER_MINUTE*MINUTES_PER_HOUR
+#define MILLISECONDS_PER_DAY MILLISECONDS_PER_HOUR*HOURS_PER_DAY
+
+#define LEAPYEAR(year) (!((year) % 4) && (((year) % 100) || !((year) % 400)))
+#define YEARSIZE(year) (LEAPYEAR(year) ? 366 : 365)
+#define YEAR0 1900
+#define EPOCH_YR   1970
+#define SECS_DAY   (24L * 60L * 60L)
+#define LONG_MAX   2147483647L
+#define TIME_MAX   0xFFFFFFFFL
+
+const int _ytab[2][12] = { { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
+                           { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }};
+
+
+
 
 //TODO: Adapter quand les accès à la clock seront faisable en user space
 
@@ -104,23 +128,177 @@ double difftime(time_t time1, time_t time0)
  */
 time_t mktime(struct tm *timep __attribute__((unused)))
 {
-	//return clock_mktime(timep);*/
-	//XXX
-	return 0;
+	long day, year;
+	int tm_year;
+	int yday, month;
+	unsigned long seconds;
+	int overflow;
+
+	timep->tm_min += timep->tm_sec / 60;
+	timep->tm_sec %= 60;
+	if (timep->tm_sec < 0) {
+			  timep->tm_sec += 60;
+			  timep->tm_min--;
+	}
+	timep->tm_hour += timep->tm_min / 60;
+	timep->tm_min = timep->tm_min % 60;
+	if (timep->tm_min < 0) {
+			  timep->tm_min += 60;
+			  timep->tm_hour--;
+	}
+	day = timep->tm_hour / 24;
+	timep->tm_hour= timep->tm_hour % 24;
+	if (timep->tm_hour < 0) {
+			  timep->tm_hour += 24;
+			  day--;
+	}
+	timep->tm_year += timep->tm_mon / 12;
+	timep->tm_mon %= 12;
+	if (timep->tm_mon < 0) {
+			  timep->tm_mon += 12;
+			  timep->tm_year--;
+	}
+	day += (timep->tm_mday - 1);
+	while (day < 0) {
+			  if(--timep->tm_mon < 0) {
+						 timep->tm_year--;
+						 timep->tm_mon = 11;
+			  }
+			  day += _ytab[LEAPYEAR(YEAR0 + timep->tm_year)][timep->tm_mon];
+	}
+	while (day >= _ytab[LEAPYEAR(YEAR0 + timep->tm_year)][timep->tm_mon]) {
+			  day -= _ytab[LEAPYEAR(YEAR0 + timep->tm_year)][timep->tm_mon];
+			  if (++(timep->tm_mon) == 12) {
+						 timep->tm_mon = 0;
+						 timep->tm_year++;
+			  }
+	}
+	timep->tm_mday = day + 1;
+	year = EPOCH_YR;
+	if (timep->tm_year < year - YEAR0) return (time_t)-1;
+	seconds = 0;
+	day = 0;                        /* means days since day 0 now */
+	overflow = 0;
+	/* Assume that when day becomes negative, there will certainly
+	 * be overflow on seconds.
+	 * The check for overflow needs not to be done for leapyears
+	 * divisible by 400.
+	 * The code only works when year (1970) is not a leapyear.
+	 */
+
+	tm_year = timep->tm_year + YEAR0;
+
+	if (LONG_MAX / 365 < tm_year - year) overflow++;
+	day = (tm_year - year) * 365;
+	if (LONG_MAX - day < (tm_year - year) / 4 + 1) overflow++;
+	day += (tm_year - year) / 4
+			  + ((tm_year % 4) && tm_year % 4 < year % 4);
+	day -= (tm_year - year) / 100
+			  + ((tm_year % 100) && tm_year % 100 < year % 100);
+	day += (tm_year - year) / 400
+			  + ((tm_year % 400) && tm_year % 400 < year % 400);
+
+	yday = month = 0;
+	while (month < timep->tm_mon) {
+			  yday += _ytab[LEAPYEAR(tm_year)][month];
+			  month++;
+	}
+	yday += (timep->tm_mday - 1);
+	if (day + yday < 0) overflow++;
+	day += yday;
+
+	timep->tm_yday = yday;
+	timep->tm_wday = (day + 4) % 7;         /* day 0 was thursday (4) */
+
+	seconds = ((timep->tm_hour * 60L) + timep->tm_min) * 60L + timep->tm_sec;
+
+	if ((TIME_MAX - seconds) / SECS_DAY < (unsigned long int)day) overflow++;
+	seconds += day * SECS_DAY;
+
+	if (overflow)
+		return (time_t)-1;
+
+	/*if ((time_t)seconds != seconds)
+		return (time_t)-1;
+	*/
+	/* affichage de debug
+	printf("mktime for\n\
+				year : %d\n\
+				mon : %d\n\
+				day : %d\n\
+				hour : %d\n\
+				min : %d\n\
+				sec : %d\n\
+				unix => %d\n\
+				",
+				timep->tm_year,
+				timep->tm_mon,
+				timep->tm_mday,
+				timep->tm_hour,
+				timep->tm_min,
+				timep->tm_sec,
+				seconds
+			);
+	*/
+	
+	return (time_t)seconds;
 }
 
-struct tm *gmtime(const time_t *timer __attribute__((unused)))
+struct tm *gmtime(const time_t *timer)
 {
-	//return clock_gmtime(timer);
-	//XXX
-	return 0;
+	static struct tm br_time;
+	register struct tm *timep = &br_time;
+	time_t time = *timer;
+	register unsigned long dayclock, dayno;
+	int year = EPOCH_YR;
+
+	dayclock = (unsigned long)time % SECS_DAY;
+	dayno = (unsigned long)time / SECS_DAY;
+
+	timep->tm_sec = dayclock % 60;
+	timep->tm_min = (dayclock % 3600) / 60;
+	timep->tm_hour = dayclock / 3600;
+	timep->tm_wday = (dayno + 4) % 7;       /* day 0 was a thursday */
+	while (dayno >= YEARSIZE(year)) {
+		dayno -= YEARSIZE(year);
+		year++;
+	}
+	timep->tm_year = year - YEAR0;
+	timep->tm_yday = dayno;
+	timep->tm_mon = 0;
+	while (dayno >= (unsigned long) _ytab[LEAPYEAR(year)][timep->tm_mon]) {
+		dayno -= _ytab[LEAPYEAR(year)][timep->tm_mon];
+		timep->tm_mon++;
+	}
+	timep->tm_mday = dayno + 1;
+	timep->tm_isdst = 0;
+
+	/* affichage de debug
+	printf("gm for\n\
+				unix => %d\n\
+				year : %d\n\
+				mon : %d\n\
+				day : %d\n\
+				hour : %d\n\
+				min : %d\n\
+				sec : %d\n\
+				",
+				*timer,
+				timep->tm_year,
+				timep->tm_mon,
+				timep->tm_mday,
+				timep->tm_hour,
+				timep->tm_min,
+				timep->tm_sec
+			);
+	*/
+	
+	return timep;
 }
 
-struct tm *localtime(const time_t *timer __attribute__((unused)))
+struct tm *localtime(const time_t *timer)
 {
-	//return gmtime(timer);
-	//XXX
-	return 0;
+	return gmtime(timer);
 }
 /*
 size_t strftime(char * s, size_t maxsize, const char * format, const struct tm * timeptr)
