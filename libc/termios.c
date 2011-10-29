8885d5be3ae4d3412f49055fe38a9c42c8e7f5af
Maxime Chéramy <maxime81@gmail.com>
2011-10-29 20:54:52 +0200

    suppression de 2 warnings.

--- libc/termios.c
+++ libc/termios.c
@@ -2,11 +2,11 @@
 #include <sys/ioctl.h>
 
 int tcgetattr(int fd, struct termios *termios_p) {
-	ioctl(fd, TCGETS, termios_p);
+	ioctl(fd, TCGETS, (void*)termios_p);
 	return 0;
 }
 
 int tcsetattr(int fd, int optional_actions, const struct termios *termios_p) {
-	ioctl(fd, TCSETS, termios_p);
+	ioctl(fd, TCSETS, (void*)termios_p);
 	return 0;
 }
