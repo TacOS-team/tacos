8b1eaad437216f4aff74168e6718aeddc2ec57a6
Maxime Chéramy <maxime81@gmail.com>
2011-11-11 16:36:48 +0100

    La commande cd demande son argument sur sa ligne de commande !

--- applications/mishell/include/shell_utils.h
+++ applications/mishell/include/shell_utils.h
@@ -1,7 +1,7 @@
 #ifndef _SHELL_UTILS_H
 #define _SHELL_UTILS_H
 
-typedef int (*func_ptr)();
+typedef int (*func_ptr)(int argc, char **argv);
 
 void add_builtin_cmd(func_ptr func, char* name);
 int exec_builtin_cmd(char* name);
