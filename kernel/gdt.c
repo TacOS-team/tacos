d14254f14295d71a373996b3f3e4f7f18ef8f112
Maxime Chéramy <maxime81@gmail.com>
2012-02-19 17:37:32 +0100

    Revert du commit n°222 du svn datant de mars 2010 (2 ans). Je ne sais pas trop pourquoi c'était commenté, visiblement pour résoudre un plantage, sauf que maintenant ça ne plante plus, donc je propose de remettre ce bout de code.

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -3,7 +3,6 @@
  *
  * @author TacOS developers 
  *
- *
  * @section LICENSE
  *
  * Copyright (C) 2010, 2011, 2012 - TacOS developers.
@@ -24,7 +23,7 @@
  *
  * @section DESCRIPTION
  *
- * Description de ce que fait le fichier
+ * @brief Setup de la GDT.
  */
 
 #include <types.h>
@@ -140,7 +139,7 @@ static struct x86_segment_descriptor gdt[] = {
 /*
  * Configuration de la gdt.
  */
-void gdt_setup(size_t ram_size __attribute__ ((unused))) {
+void gdt_setup(size_t ram_size) {
 	struct x86_gdt_register gdtr;
 
 	/* Adresse de la GDT */
@@ -151,17 +150,17 @@ void gdt_setup(size_t ram_size __attribute__ ((unused))) {
 	
 	/* On utilise des segments dont la limite correspond à la mémoire physique ! */
 
-	/*
-	gdt[1].segment_limit_15_0 = (ram_size / PAGE_SIZE + 1) & 0xffff;
-	gdt[1].segment_limit_19_16 = ((ram_size / PAGE_SIZE + 1) >> 16) & 0xf;
-	gdt[2].segment_limit_15_0 = (ram_size / PAGE_SIZE + 1) & 0xffff;
-	gdt[2].segment_limit_19_16 = ((ram_size / PAGE_SIZE + 1) >> 16) & 0xf;
-	
-	gdt[3].segment_limit_15_0 = (ram_size / PAGE_SIZE + 1) & 0xffff;
-	gdt[3].segment_limit_19_16 = ((ram_size / PAGE_SIZE + 1) >> 16) & 0xf;
-	gdt[4].segment_limit_15_0 = (ram_size / PAGE_SIZE + 1) & 0xffff;
-	gdt[4].segment_limit_19_16 = ((ram_size / PAGE_SIZE + 1) >> 16) & 0xf;
-	*/
+	if (ram_size > 0) {
+		gdt[1].segment_limit_15_0 = (ram_size / PAGE_SIZE + 1) & 0xffff;
+		gdt[1].segment_limit_19_16 = ((ram_size / PAGE_SIZE + 1) >> 16) & 0xf;
+		gdt[2].segment_limit_15_0 = (ram_size / PAGE_SIZE + 1) & 0xffff;
+		gdt[2].segment_limit_19_16 = ((ram_size / PAGE_SIZE + 1) >> 16) & 0xf;
+		
+		gdt[3].segment_limit_15_0 = (ram_size / PAGE_SIZE + 1) & 0xffff;
+		gdt[3].segment_limit_19_16 = ((ram_size / PAGE_SIZE + 1) >> 16) & 0xf;
+		gdt[4].segment_limit_15_0 = (ram_size / PAGE_SIZE + 1) & 0xffff;
+		gdt[4].segment_limit_19_16 = ((ram_size / PAGE_SIZE + 1) >> 16) & 0xf;
+	}
 
 	/* Initialisation du descripteur de TSS dans la GDT */
 	gdt[5].base_address_15_0 = ((uint32_t)(&default_tss) & 0xffff);
