7c7c847754d9a56f012ecbf6453f6824af8f2e47
Maxime Chéramy <maxime81@gmail.com>
2015-06-22 19:09:17 +0200

    getty plante moins facilement...

--- applications/getty.c
+++ applications/getty.c
@@ -10,20 +10,25 @@ int main(int argc, char **argv) {
 
 	//printf("Ouverture de %s\n", argv[1]);
 
-	// On ferme les descripteurs actuels.
-	fclose(stdin);
-	fclose(stdout);
-	fclose(stderr);
-	
-	stdin = fopen(argv[1], "r");
-	stdout = fopen(argv[1], "w");
-	stderr = fopen(argv[1], "w");
-
 	int cttyfd = open(argv[1], O_RDWR);
-	ioctl(cttyfd, TIOCSCTTY, argv[1]);
-	close(cttyfd);
+	if (cttyfd >= 0) {
+		// On ferme les descripteurs actuels.
+		fclose(stdin);
+		fclose(stdout);
+		fclose(stderr);
 
-	exec_elf("/tacos/bin/mishell");
+		stdin = fopen(argv[1], "r");
+		stdout = fopen(argv[1], "w");
+		stderr = fopen(argv[1], "w");
+
+		cttyfd = open(argv[1], O_RDWR);
+		ioctl(cttyfd, TIOCSCTTY, argv[1]);
+		close(cttyfd);
+
+		exec_elf("/tacos/bin/mishell");
+	} else {
+		fprintf(stderr, "Erreur à l'ouverture de %s.\n", argv[1]);
+	}
 
 	return 0;
 }
